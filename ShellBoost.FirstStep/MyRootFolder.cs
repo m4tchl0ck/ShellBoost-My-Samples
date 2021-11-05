3b674649da731d622ac18a50b6d988fc23648faf
m4tchl0ck <m4tchl0ck@outlook.com>
2021-11-05 15:25:38 +0100

    Add context menu item

--- ShellBoost.FirstStep/MyRootFolder.cs
+++ ShellBoost.FirstStep/MyRootFolder.cs
@@ -1,6 +1,7 @@
 using ShellBoost.Core;
 using ShellBoost.Core.WindowsShell;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ShellBoost.FirstStep
 {
@@ -17,6 +18,23 @@ namespace ShellBoost.FirstStep
             // note by default, ShellBoost uses the key/ID value as the display name if it’s not explicitly defined
             yield return new ShellFolder(this, new StringKeyShellItemId("My First Folder"));
             yield return new ShellItem(this, new StringKeyShellItemId("My First Item"));
+        }
+
+        protected override void MergeContextMenu(ShellFolder folder, IReadOnlyList<ShellItem> items, ShellMenu existingMenu, ShellMenu appendMenu)
+        {
+            if (items.Count == 1 && !items[0].IsFolder)
+            {
+                // this is one shell item, do something else
+            }
+            else
+            {
+                // tags are placeholder for anything. we use them to remember what's the real command behind the menu item
+                if (existingMenu.Items.FirstOrDefault(i => i.Text == "New") == null)
+                {
+                    var newItem = new ShellMenuItem(appendMenu, "New Item");
+                    appendMenu.Items.Add(newItem);
+                }
+            }
         }
     }
 }
