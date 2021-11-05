4e69dff5cd1dc38fbbb0c89051ce065e49a11bfc
m4tchl0ck <m4tchl0ck@outlook.com>
2021-11-05 15:35:05 +0100

    Add sync folder to extention folder

--- ShellBoost.FirstStep/MyRootFolder.cs
+++ ShellBoost.FirstStep/MyRootFolder.cs
@@ -1,6 +1,7 @@
 using ShellBoost.Core;
 using ShellBoost.Core.WindowsShell;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace ShellBoost.FirstStep
@@ -12,12 +13,17 @@ namespace ShellBoost.FirstStep
             : base(idList)
         {
         }
-
         public override IEnumerable<ShellItem> EnumItems(SHCONTF options)
         {
             // note by default, ShellBoost uses the key/ID value as the display name if it’s not explicitly defined
             yield return new ShellFolder(this, new StringKeyShellItemId("My First Folder"));
             yield return new ShellItem(this, new StringKeyShellItemId("My First Item"));
+            yield return new OnDemandFolder(this);
+
+            foreach (var shellItem in base.EnumItems(options))
+            {
+                yield return shellItem;
+            }
         }
 
         protected override void MergeContextMenu(ShellFolder folder, IReadOnlyList<ShellItem> items, ShellMenu existingMenu, ShellMenu appendMenu)
@@ -34,6 +40,27 @@ namespace ShellBoost.FirstStep
                     var newItem = new ShellMenuItem(appendMenu, "New Item");
                     appendMenu.Items.Add(newItem);
                 }
+            }
+        }
+    }
+
+    public class OnDemandFolder : ShellFolder
+    {
+        public OnDemandFolder(ShellFolder parent)
+            : base(parent, new DirectoryInfo($@"C:\tmp\somePath"))
+        {
+
+        }
+
+        protected override void MergeContextMenu(ShellFolder folder, IReadOnlyList<ShellItem> items, ShellMenu existingMenu, ShellMenu appendMenu)
+        {
+            base.MergeContextMenu(folder, items, existingMenu, appendMenu);
+
+            // tags are placeholder for anything. we use them to remember what's the real command behind the menu item
+            if (existingMenu.Items.FirstOrDefault(i => i.Text == "New") == null)
+            {
+                var newItem = new ShellMenuItem(appendMenu, "New");
+                appendMenu.Items.Add(newItem);
             }
         }
     }
