34673b384e6d898d075596b68ad79c207d822db8
m4tchl0ck <m4tchl0ck@outlook.com>
2021-11-12 11:11:23 +0100

    Do not init ShellFolderServer

--- ShellBoost.FirstStep/MyService.cs
+++ ShellBoost.FirstStep/MyService.cs
@@ -9,23 +9,16 @@ namespace ShellBoost.FirstStep
 {
     public class MyService : IHostedService
     {
-        private readonly ShellFolderServer _shellFolderServer;
-        private readonly ShellFolderConfiguration _shellFolderConfiguration;
-        private MultiPointSynchronizer _mps;
+        private readonly MultiPointSynchronizer _mps;
 
         public MyService()
         {
-            _shellFolderServer = new MyShellFolderServer();
-            _shellFolderConfiguration = new ShellFolderConfiguration();
-
             // create the MPS. We need a globally unique identifier (could be a guid)
             _mps = new MultiPointSynchronizer("MyCompany.MyProgram");
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            ShellFolderServer.RegisterNativeDll(RegistrationMode.User);
-
             var onDemandRootPath = @"c:\tmp\somePath";
 
             // Register the File On-Demand root, use an end-user friendly display name.
@@ -44,7 +37,6 @@ namespace ShellBoost.FirstStep
             // start the MPS
             _mps.Start();
 
-            _shellFolderServer.Start(_shellFolderConfiguration); // start the server
             Console.WriteLine("Started.");
 
             return Task.CompletedTask;
@@ -52,11 +44,9 @@ namespace ShellBoost.FirstStep
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            _shellFolderServer.Stop();
-            _shellFolderServer.Dispose();
             _mps.Stop();
             _mps.Dispose();
-            ShellFolderServer.UnregisterNativeDll(RegistrationMode.User);
+
             Console.WriteLine("Stoped.");
 
             return Task.CompletedTask;
